0f2c830ab6147bb86767a54180079ab6ceea6a4e
Nipun Hedaoo <nipunhedaoo@gmail.com>
2022-03-18 05:11:30 +0530

    home controller test

--- build.sbt
+++ build.sbt
@@ -13,15 +13,11 @@ libraryDependencies += ehcache
 
 libraryDependencies ++= Seq(
   javaWs,
-  "org.json" % "org.json" % "chargebee-1.0"
+  "org.json" % "org.json" % "chargebee-1.0",
+  "org.mockito" % "mockito-core" % "4.0.0" % Test,
+  "org.powermock" % "powermock-module-junit4" % "2.0.9" % Test,
+  "org.powermock" % "powermock-api-mockito2" % "2.0.9" % Test,
 )
-
-
-libraryDependencies += "org.mockito" % "mockito-core" % "4.0.0" % Test
-
-libraryDependencies += "org.powermock" % "powermock-module-junit4" % "2.0.9" % Test
-
-libraryDependencies += "org.powermock" % "powermock-api-mockito2" % "2.0.9" % Test
 
 routesGenerator := InjectedRoutesGenerator
 
--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,18 +1,47 @@
 package controllers;
 
+import models.ProjectDetails;
+import models.SearchResultModel;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
+import play.libs.ws.WSResponse;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.GET;
 import static play.test.Helpers.route;
 
+import services.FreeLancerServices;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import play.cache.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.*;
+
 public class HomeControllerTest extends WithApplication {
+
+    @Mock
+    FreeLancerServices freeLancerServices;
+    @Mock
+    AsyncCacheApi cache;
+
+    @InjectMocks
+    HomeController homeController;
+
 
     @Override
     protected Application provideApplication() {
@@ -39,5 +68,33 @@ public class HomeControllerTest extends WithApplication {
         assertEquals(OK, result.status());
     }
 
+    @Test
+    public void searchResultsTest()
+    {
+        running(provideApplication(), () -> {
+            when(cache.getOrElseUpdate(any(),any())).thenReturn(repositoryDetailsObject());
+
+            CompletionStage<WSResponse> repositoryDetails = freeLancerServices.searchResults("play");
+            try {
+                repositoryDetails.toCompletableFuture().get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            assertTrue(repositoryDetails.toCompletableFuture().isDone());
+        });
+    }
+
+    private CompletionStage<Object> repositoryDetailsObject(){
+        return CompletableFuture.supplyAsync( () -> {
+            SearchResultModel searchResult = new SearchResultModel();
+            List<ProjectDetails> projectDets = new ArrayList<>();
+            searchResult.setprojectDetails(projectDets);
+            searchResult.setfleschKincaidGradeLevel(0.0);
+            searchResult.setfleschReadabilityIndex(0.0);
+            return searchResult;
+        });
+    }
 
 }
